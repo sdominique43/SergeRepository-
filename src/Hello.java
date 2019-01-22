e15e3ac45b003aa656f9296fa9dfae95a52932d0
dominiques705 <dominiques705@ALB205-24.strose.edu>
2019-01-22 15:48:28 -0500

    Update

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
+import java.util.*;
 
 public class Hello {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello");
+		
+		//Take in the user input
+		Scanner keyboard = new Scanner(System.in);
+		
+		
 	}
 }
