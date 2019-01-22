a200856d5dfc7f507bc8ce3d354eb73595459707
dominiques705 <dominiques705@CISSUITE2.strose.edu>
2019-01-22 16:24:18 -0500

    Hello

--- src/Hello.java
+++ src/Hello.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class Hello {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
+		System.out.println("Hello, who's am I speaking to?");
 		
 		//Take in the user input
 		Scanner keyboard = new Scanner(System.in);
 		
+		String inputString = keyboard.nextLine();
 		
+		System.out.println(inputString);
 	}
 }
